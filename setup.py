c395d7b041c59e8c55c509936839ebfcf43d4060
Yselessia <147167078+Yselessia@users.noreply.github.com>
2025-08-13 12:16:14 +0100

    Remove tracked files/folders now ignored.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ao3_api',
+    version='0.1',
+    packages=find_packages(),
+    description='An unofficial AO3 API for python.',
+    author='wendytg, Yselessia',
+    author_email='help.yselessia@gmail.com',
+    url='https://github.com/Yselessia/ao3_api',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',
+)
